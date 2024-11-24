9f801e5e9bf1c353e28d9a333db7a303a7cc20fa
Arjav Barya <arjavbarya@Arjavs-MacBook-Air.local>
2024-11-24 16:59:03 +0530

    checking width

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -136,7 +136,15 @@ function ResponsiveDrawer(props) {
     const container = window !== undefined ? () => window().document.body : undefined;
 
     return (
-        <Box sx={{ display: 'flex'}}>
+        <Box
+        sx={{
+            display: 'flex',
+            maxWidth: '100%',
+            mx: 'auto', // Centers the container horizontally
+            width: { sm: 'calc(100% - 250px)', xs: '100%' },
+            // px: { xs: 4, xl: 24 }, // Horizontal padding for small and extra-large screens
+        }}
+    >
             <CssBaseline />
             <AppBar
                 position="fixed"
